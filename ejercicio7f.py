31ee959487b4c29e4002502845a39cec21b2ee00
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-07 16:04:43 -0500

    Create ejercicio7f.py

---
+++ ejercicio7f.py
@@ -0,0 +1,15 @@
+#3from 7ef import *
+print("EJERCICIO 7 conversion de numeros decimal-binario/binario-decimal")
+
+print("")
+
+
+
+numd = int(input("Ingresa un número decimal: "))
+print(f"El número {numd} convertido a binario es: {to_bin(numd)}")
+
+print("")
+
+numb = int(input("Ingresa un número binario: "))
+print(f"El número {numb} convertido a decimal es: {to_dec(numb)}")
+
