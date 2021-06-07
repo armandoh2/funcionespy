d1ce54268a59e7f1d68823363fe48116fa103278
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-07 13:58:46 -0500

    Update ejercicio1f.py

--- ejercicio1f.py
+++ ejercicio1f.py
@@ -6,12 +6,12 @@ from 1ef import *
 print("EJERCICIO 1 calcular el IVA")
 
 
-precio = int(input("Ingresa la cantidad de la factura sin IVA: $"))
-iva = input("Ingresa el porcentage del IVA para aplicar: ")
+precio = int(input("Ingresa la cantidad sin IVA: $"))
+iva = input("Ingresa el porcentaje del IVA : ")
 print("")
 if iva == "":
   iva = 21
 facturar(precio,iva)
-print(f"La factuación agregando el {iva}% de IVA es: ${facturar(precio,iva)}")
+print(f"La factuación con IVA es: ${facturar(precio,iva)}")
 
 
