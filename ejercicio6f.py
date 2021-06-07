2e10d9180afdd03db5671bd368bd1fdf81d997f7
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-07 15:57:33 -0500

    Create ejercicio6f.py

---
+++ ejercicio6f.py
@@ -0,0 +1,12 @@
+#3from 6ef import 
+print(" EJERCICIO 6 calcular maximo comun divisor y minimo comun multiplo")
+
+
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
+
+print("")
+
+print(f"El máximo común divisor  es: {calculo_mcd(numero1,numero2)}")
+print(f"El mínimo común múltiplo  es: {calculo_mcm(numero1,numero2)}")
+print("")
