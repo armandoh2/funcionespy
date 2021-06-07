6b68cc8db6d6d352f1ad01ebfc5194d1cfca3588
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-07 14:06:39 -0500

    Create ejercicio2f.py

---
+++ ejercicio2f.py
@@ -0,0 +1,10 @@
+
+print("EJERCICIO 2 calcular area y volumen circulo y cilindro")
+print("")
+radio = int(input("Ingresa el readio del circulo: "))
+altura = int(input("Ingresa la altura del cilindro: "))
+print("")
+print(f"El área del Circulo, es {calculo_area(radio)}")
+radio = calculo_area(radio)
+print(f"El volumen del Cilindro es "{calculo_volumen(altura,radio)}")
+
