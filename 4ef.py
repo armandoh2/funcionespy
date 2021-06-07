7ccd2973e3a88354b402f9710ac35518e3ec406f
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-07 14:25:01 -0500

    Create 4ef.py

---
+++ 4ef.py
@@ -0,0 +1,10 @@
+#ejercicio 4 calcular el cuadrado
+
+
+def calculo_cuadrados(listanc,listarc):
+  listarc[0] = listanc[0]**2
+  listarc[1] = listanc[1]**2
+  listarc[2] = listanc[2]**2
+  listarc[3] = listanc[3]**2
+  listarc[4] = listanc[4]**2
+  return listarc
