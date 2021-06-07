cefb5bd93654921496d1965f1062457fecd9f138
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-07 15:51:13 -0500

    Create ejercicio5f.py

---
+++ ejercicio5f.py
@@ -0,0 +1,16 @@
+
+print("EJERCICIO 5 calcular datos estadisticos de 5 numeros ")
+
+print("")
+
+listade = [0,0,0,0,0]
+
+listade[0] = int(input("Ingresa el primer número: "))
+listade[1] = int(input("Ingresa el segundo número: "))
+listade[2] = int(input("Ingresa el tercer número: "))
+listade[3] = int(input("Ingresa el cuarto número: "))
+listade[4] = int(input("Ingresa el quinto número: "))
+
+print("")
+
+calculo_estadistico(listade)
